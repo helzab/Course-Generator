e9c5bcd088916db96cba93d8293a559be9bf9086
Amelia Jochna <amei70bieber@gmail.com>
2024-06-19 21:33:01 +0200

    Converting script added

---
+++ convert.py
@@ -0,0 +1,15 @@
+import openpyxl
+
+def read_xlsx_and_write_to_txt(xlsx_path, txt_path):
+    workbook = openpyxl.load_workbook(xlsx_path)
+    sheet = workbook.active
+    
+    with open(txt_path, 'w') as txt_file:
+        for row in sheet.iter_rows(values_only=True):
+            row_string = ','.join([str(cell) if cell is not None else '' for cell in row])
+            txt_file.write(row_string + '\n')
+
+xlsx_path = 'subjectslist.xlsx'
+txt_path = 'TXTsubjectslist.txt'
+
+read_xlsx_and_write_to_txt(xlsx_path, txt_path)
